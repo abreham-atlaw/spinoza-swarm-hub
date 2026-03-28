7142d6b1556287c54ff585a288914f07f1f01427
Abreham Atlaw <abreham.atlaw@yahoo.com>
2026-03-28 21:18:57 +0300

    - Changed ping configs for server

--- src/python/main/swarm_hub/swarm_hub/sio.py
+++ src/python/main/swarm_hub/swarm_hub/sio.py
@@ -1,3 +1,3 @@
 import socketio
 
-sio = socketio.Server(async_mode="gevent", max_http_buffer_size=50*1024*1024)
+sio = socketio.Server(async_mode="gevent", max_http_buffer_size=50*1024*1024, ping_timeout=20, ping_interval=25)
